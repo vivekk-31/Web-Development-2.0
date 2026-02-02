a46862c8fb6829cf70eddea3d7c3faff701ce88b
vivekk-31 <viveksingh42356@gmail.com>
2026-02-02 16:03:19 +0530

    Worked on the logic of factorial

--- JavaScript/script11.js
+++ JavaScript/script11.js
@@ -1,3 +1,15 @@
-console.log("DOM Elements.")
+// console.log("DOM Elements.")
 
-console.log(document.body.firstElementChild.children);
+// console.log(document.body.firstElementChild.children);
+
+function Factorial(number) {
+    let fact = 1;
+    for (let i = 1; i <=number; i++) {
+        fact = fact*i;
+        console.log(fact)
+        
+    }
+    return fact
+}
+
+console.log(Factorial(5))
