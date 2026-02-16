3092dc28a236501fe60a36fc25accf8771c0d842
vivekk-31 <viveksingh42356@gmail.com>
2026-02-16 16:10:12 +0530

    Working on Dynamic Card Builder

---
+++ JavaScript/script14.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    //Finish this function
+}
+
+createCard("Sigma Web Development | Introduction to Backend", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
