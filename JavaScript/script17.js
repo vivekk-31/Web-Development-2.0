7e098f61abe9733cb851e4c61bdfb2d7d02ee08d
vivekk-31 <viveksingh42356@gmail.com>
2026-03-15 23:51:19 +0530

    Learning async

---
+++ JavaScript/script17.js
@@ -0,0 +1,16 @@
+// console.log("Vivek is a hacker.")
+// console.log("Roman is a hecker.")
+
+// setTimeout(() => {
+//     console.log("Setimeout1")
+// }, 0);
+
+// console.log("The end.")
+
+function demo() {
+    console.log("Hello")
+}
+
+let a = demo;
+
+a();
