48d8e09975fd9489ab88e066c51e694cd40a5e2c
vivekk-31 <viveksingh42356@gmail.com>
2026-03-21 23:11:32 +0530

    Learning Promises

--- JavaScript/script19.js
+++ JavaScript/script19.js
@@ -1,15 +1,32 @@
-function loadScript(src, callback){
+// function loadScript(src, callback){
+//     let script = document.createElement("script");
+//     script.src = src;
+
+//     script.onload = ()=>callback(script)
+
+//     document.head.append(script)
+
+// }
+
+
+
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/script.js/2.0.2/script.min.js", (script)=>{
+//     alert("script is loaded")
+// })
+
+//Same thing using promises:
+
+let prom1 = new Promise((resolve, reject)=>{
     let script = document.createElement("script");
     script.src = src;
+    script.onload(resolve(script))
+    script.onerror(reject(console.log("Error loading the script")))
+        document.head.append(script)
 
-    script.onload = ()=>callback(script)
-
-    document.head.append(script)
-
-}
-
-
-
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/script.js/2.0.2/script.min.js", (script)=>{
-    alert("script is loaded")
 })
+
+prom1.then((a)=>{
+    console.log(a)
+})
+
+
