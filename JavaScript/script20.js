e23fceacea6da815fd2a32f7a1795a90ac9b3096
vivekk-31 <viveksingh42356@gmail.com>
2026-03-28 23:58:58 +0530

    Learning error handling

---
+++ JavaScript/script20.js
@@ -0,0 +1,20 @@
+let a = prompt("Enter first number");
+let b = prompt("Enter second number");
+
+if (isNaN(a) || isNaN(b)) {
+    throw new Error("This is not allowed");
+}
+
+let sum = parseInt(a) + parseInt(b)
+
+try {
+    console.log("The sum is " +sum*x)
+} catch (error) {
+    console.log("Error successful")
+}
+
+
+// console.log('The sum is ', + sum);
+
+
+
