74ea8ed8ed659306aa908294c4b205244d64a2c2
vivekk-31 <viveksingh42356@gmail.com>
2026-04-01 15:08:03 +0530

    Working on Double Trouble

---
+++ JavaScript/JS Problems/TheDoubleTrouble.js
@@ -0,0 +1,16 @@
+// The Double Trouble:
+//    You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
+
+function double() {
+    let arr = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8]
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        // console.log(element)
+
+        
+    }
+    
+}
+
+double()
