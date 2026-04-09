695a11309f7b49cbea0a672bf65355e0490fb4d3
vivekk-31 <viveksingh42356@gmail.com>
2026-04-09 14:42:14 +0530

    Completed the double trouble

--- JavaScript/JS Problems/TheDoubleTrouble.js
+++ JavaScript/JS Problems/TheDoubleTrouble.js
@@ -4,24 +4,15 @@
 function double() {
     let arr = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8]
     let newArr = []
-    let newArr2 = []
     for (let i = 0; i < arr.length; i++) {
         const element = arr[i];
 
-        if (arr[i]==arr[i+1]) {
-            continue
+       if(arr[i] !==arr[i - 1]) {
+            newArr.push(element * 2)
         }
-        else{
-            newArr.push(element)
-        }
-        
     }
-    for(i=0; i<newArr.length; i++){
-        const element2 = newArr[i];
-        newArr2.push(element2*2)
-    }
-    console.log(newArr2)
-    
+    console.log(newArr)
+
 }
 
 double()
