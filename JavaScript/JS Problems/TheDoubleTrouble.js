7d5d18cf6b5218e40b959a586c0d872632ed4f33
vivekk-31 <viveksingh42356@gmail.com>
2026-04-09 14:52:22 +0530

    Solving for an unsorted array

--- JavaScript/JS Problems/TheDoubleTrouble.js
+++ JavaScript/JS Problems/TheDoubleTrouble.js
@@ -1,18 +1,32 @@
 // The Double Trouble:
 //    You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
 
-function double() {
-    let arr = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8]
-    let newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
+// function double() {
+//     let arr = [1, 2, 3, 4, 4, 5, 6, 7, 7, 8]
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
 
-       if(arr[i] !==arr[i - 1]) {
-            newArr.push(element * 2)
+//        if(arr[i] !==arr[i - 1]) {
+//             newArr.push(element * 2)
+//         }
+//     }
+//     console.log(newArr)
+
+// }
+
+// double()
+
+
+function double() {
+    let arr = [1, 4, 2, 4, 3, 2];
+    
+    for(i = 0; i<arr.length; i++){
+        let element = arr[i];
+        if (arr[i]>arr[i+1]) {
+            
         }
     }
-    console.log(newArr)
-
 }
 
 double()
