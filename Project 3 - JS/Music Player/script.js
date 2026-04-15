f17fc943c32ac4e34901e87a2b5e05fa67bb8f11
vivekk-31 <viveksingh42356@gmail.com>
2026-04-15 15:01:22 +0530

    Created audio instance

--- Project 3 - JS/Music Player/script.js
+++ Project 3 - JS/Music Player/script.js
@@ -1,6 +1,10 @@
 console.log('Let us begin.');
-
-function playMusic(){
 let audio = new Audio('songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3')
-let play = document.querySelector('.coverImage')
+
+let song = document.querySelector('.coverImage')
+
+function playMyusic(){
+audio.play()
 }
+
+song.addEventListener('click', playMyusic)
