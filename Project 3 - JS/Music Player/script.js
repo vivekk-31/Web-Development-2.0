1ffcef0734a4f30229024794a36c96936022c5d1
vivekk-31 <viveksingh42356@gmail.com>
2026-04-16 15:08:08 +0530

    Worked on the logic of loading songs

--- Project 3 - JS/Music Player/script.js
+++ Project 3 - JS/Music Player/script.js
@@ -1,10 +1,11 @@
 console.log('Let us begin.');
-let audio = new Audio('songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3')
+let audio = new Audio();
+let songs = [{name:"Tum se Hi", file:"songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3"},
+    {name:"Tune Jo Na Kaha", file:"songs/Tune Jo Na Kaha - Mohit Chauhan.mp3"}
+]
 
-let song = document.querySelector('.coverImage')
-
-function playMyusic(){
-audio.play()
+function loadSong(){
+    for (const element of object) {
+        
+    }
 }
-
-song.addEventListener('click', playMyusic)
