2ab5cefead06175cd260590bb529cea7c10419ce
vivekk-31 <viveksingh42356@gmail.com>
2026-04-23 15:01:21 +0530

    Working on the music player

--- Project 3 - JS/Music Player/script.js
+++ Project 3 - JS/Music Player/script.js
@@ -4,8 +4,12 @@ let songs = [{name:"Tum se Hi", file:"songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3"
     {name:"Tune Jo Na Kaha", file:"songs/Tune Jo Na Kaha - Mohit Chauhan.mp3"}
 ]
 
+var card = document.getElementsByClassName("card");
+
 function loadSong(){
-    for (const element of object) {
-        
-    }
+    
+}
+
+function playSong(){
+    
 }
