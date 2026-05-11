41f522b1f58dfebacbbb24f870172821858a9c5f
vivekk-31 <viveksingh42356@gmail.com>
2026-05-11 14:43:45 +0530

    Restarted again with js part of Geet

--- Project 3 - JS/Music Player/script.js
+++ Project 3 - JS/Music Player/script.js
@@ -4,12 +4,20 @@ let songs = [{name:"Tum se Hi", file:"songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3"
     {name:"Tune Jo Na Kaha", file:"songs/Tune Jo Na Kaha - Mohit Chauhan.mp3"}
 ]
 
-var card = document.getElementsByClassName("card");
+var card = document.querySelector(".card");
 
-function loadSong(){
-    
+card.addEventListener("click", ()=>{
+    console.log("Song is clicked")
+
+    loadSong(0)
+    playSong()
+})
+
+function loadSong(index){
+    audio.src = songs[index].file;
+    audio.load()
 }
 
 function playSong(){
-    
+    audio.play();
 }
