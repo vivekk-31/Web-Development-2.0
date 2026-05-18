bae47f694cd308e3533d48b992f1d9753c5af55f
vivekk-31 <viveksingh42356@gmail.com>
2026-05-18 14:27:41 +0530

    Worked on the load song logic

--- Project 3 - JS/Music Player/script.js
+++ Project 3 - JS/Music Player/script.js
@@ -1,23 +1,51 @@
 console.log('Let us begin.');
 let audio = new Audio();
-let songs = [{name:"Tum se Hi", file:"songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3"},
-    {name:"Tune Jo Na Kaha", file:"songs/Tune Jo Na Kaha - Mohit Chauhan.mp3"}
+let songs = [{ name: "Tum se Hi", file: "songs/Tum_Se_Hi_(Jab_We_Met)_320_Kbps.mp3" },
+{ name: "Tune Jo Na Kaha", file: "songs/Tune Jo Na Kaha - Mohit Chauhan.mp3" },
+{ name: "Faasle", file: "songs/Faasle.mp3" },
+{ name: "Hain Junoon", file: "songs/Hai Junoon.mp3" }
 ]
 
-var card = document.querySelector(".card");
+var songsDisplay = document.querySelector(".songsDisplay")
+songs.forEach((currentItem, index) => {
+    console.log(index)
 
-card.addEventListener("click", ()=>{
-    console.log("Song is clicked")
+    let cards = document.createElement("div");
+    cards.className = "card"
+    let coverImage = document.createElement("div")
+    coverImage.className = "coverImage"
+    let img = document.createElement("img")
+    img.src = "songs/cover.jpg"
+    img.alt = "coverImage"
+    coverImage.appendChild(img)
+    let songTitle = document.createElement("div")
+    songTitle.className = "songTitle"
+    let title = document.createElement("p")
+    title.className = "title"
+    songTitle.appendChild(title)
+    title.innerText = currentItem.name;
+    cards.appendChild(coverImage)
+    coverImage.appendChild(img)
+    songTitle.appendChild(cards)
 
-    loadSong(0)
-    playSong()
-})
+    // let card = document.querySelector(".card")
+    cards.addEventListener("click", () => {
+        loadSong(index)
+        playSong()
+    })
 
-function loadSong(index){
+    songsDisplay.appendChild(cards);
+
+});
+
+
+
+
+function loadSong(index) {
     audio.src = songs[index].file;
     audio.load()
 }
 
-function playSong(){
+function playSong() {
     audio.play();
 }
